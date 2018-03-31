634ecc08bf41a9bd98d54214fe6e57ce6da1ba21
cobweb <codename.cobweb@gmail.com>
2018-03-31 15:47:43 +0300

    handlebars

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
 const express = require('express');
 const app = express();
 var fortune = require('./lib/fortune.js');
-
+var weather = require('./lib/weather');
+var express_handlebars_sections = require('express-handlebars-sections');
 const PORT = 3000;
 
 app.set('port', process.env.PORT || PORT);
-
+app.set('view cache', false);
 // Установка механизма представления handlebars
-var handlebars = require('express-handlebars').create({
-    defaultLayout: 'main'
-});
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+express_handlebars_sections(handlebars);
 
 app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
-    res.locals.showTests = app.get('env') !== 'production' &&
-        req.query.test === '1';
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
 });
+app.use(function (req, res, next) {
+    if (!res.locals.partials) 
+        res.locals.partials = {};
+    res.locals.partials.weatherContext = weather.getWeatherData();
+    next();
+});
+
 // Отключить заголовки
 app.disable('x-powered-by');
 
 app.get('/', function (req, res) {
     res.render('home');
+});
+
+app.get('/jquery-test', function (req, res) {
+    res.render('jquery-test');
 });
 
 app.get('/about', function (req, res) {
@@ -41,11 +51,19 @@ app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
 });
 
+app.get('/nursery-rhyme', function (req, res) {
+    res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function (req, res) {
+    res.json({animal: 'squirrel', bodyPart: 'tail', adjective: 'bushy', noun: 'heck'});
+});
+
 // Set headers
 app.get('/headers', function (req, res) {
     res.set('Content-Type', 'text/plain');
     var s = '';
-    for (var name in req.headers)
+    for (var name in req.headers) 
         s += name + ': ' + req.headers[name] + '\n';
     res.send(s);
 });
@@ -64,9 +82,5 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(app.get('port'), function () {
-    console.log(
-        'Express запущен на http://localhost:' +
-        app.get('port') +
-        '; нажмите Ctrl+C для завершения.'
-    );
+    console.log('Express запущен на http://localhost:' + app.get('port') + '; нажмите Ctrl+C для завершения.');
 });
