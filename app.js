43c8dec85a918130ed9231f54756375f6b63bc34
cobweb <codename.cobweb@gmail.com>
2018-03-31 09:17:19 +0300

    fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var fortune = require('./lib/fortune.js');
 
 const PORT = 3000;
 
@@ -7,47 +8,65 @@ app.set('port', process.env.PORT || PORT);
 
 // Установка механизма представления handlebars
 var handlebars = require('express-handlebars').create({
-  defaultLayout: 'main'
+    defaultLayout: 'main'
 });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-// пользовательская страница 404
-app.use(function(req, res) {
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 — Не найдено');
+app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+// Отключить заголовки
+app.disable('x-powered-by');
+
+app.get('/', function (req, res) {
+    res.render('home');
 });
 
-// пользовательская страница 500
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.type('text/plain');
-  res.status(500);
-  res.send('500 — Ошибка сервера');
+app.get('/about', function (req, res) {
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
-app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
 });
 
-app.get('/about', function(req, res) {
-  res.type('text/plain');
-  res.send('О Meadowlark Travel');
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
 });
 
-// пользовательская страница 404
-app.use(function(req, res, next) {
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 — Не найдено');
+// Set headers
+app.get('/headers', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers)
+        s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
 });
 
-app.listen(app.get('port'), function() {
-  console.log(
-    'Express запущен на http://localhost:' +
-      app.get('port') +
-      '; нажмите Ctrl+C для завершения.'
-  );
+// Обобщенный обработчик 404 (промежуточное ПО)
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+// Обработчик ошибки 500 (промежуточное ПО)
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+    console.log(
+        'Express запущен на http://localhost:' +
+        app.get('port') +
+        '; нажмите Ctrl+C для завершения.'
+    );
 });
